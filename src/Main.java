bd4494f25dcc623480776a1b41536b28cda1cdaf
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-11-09 08:13:08 -0500

    Secret phrasing

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,18 @@ public class Main {
         int wordToGuess;
         int WordToGuessLength;
         int lettersRemaining;
-
-        String[] wordsToGuessArray = new String[]{"progress","programming","cat","dog","shackle"};
-
-
+        String secretPhrase;
+        String[] guessPhraseBreakDown = secretPhrase.split("");
 
 
-        System.out.println("Welcome to hangman!\n In this game, you and another player, " +
-                "if wanted, wil play against each other." +
+
+        System.out.println("Welcome to hangman!\n In this game, you and another player " +
+                "wil play against each other." +
                 "\n One of you will guess the word while the other will be the one who gives the word. \n\n" +
                 "Ready to play?");
         keyboard.nextLine();
-        System.out.println();
+        System.out.println("Player 2,  please input the word to guess. Player 1 please turn around");
+        secretPhrase = keyboard.nextLine().toLowerCase();
         hangmanDisplay();
 
     }
@@ -42,5 +42,8 @@ public class Main {
         System.out.println("--------------------------------");
 
     }
+ //   public static String replaceSecretWordDashes(String secretWord){
+
+  //  }
 
 }
